5d252a7fc17095a44cf06010b8b7e7fdf2752ad9
“[Razin <anik@agile1tech.com>
2022-05-07 14:13:24 -0400

    Java class - 1

--- JavaLearning/src/package1/helloWorld.java
+++ JavaLearning/src/package1/helloWorld.java
@@ -4,8 +4,20 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		
+		// declaration and initialization of variable 
+		int abc = 134 ;
 		
-		// TODO Auto-generated method stub
+		// declare a variable 
+		int number2;
+		
+		// initialize a variable
+		number2=34;
+		
+		
+		
+		
+		
+		//System.out.println(abc);
 
 	}
 
--- JavaLearning/src/package1/testRunOne.java
+++ JavaLearning/src/package1/testRunOne.java
@@ -1,17 +1,31 @@
 package package1;
 
 public class testRunOne {
-	
-	
-	
-	
-	// code here 
-	
-	
-	
-	
-	
-	
+	public static void main ( String args[]) {
+		
+		// numberone is an interger type of variable but there is 
+		// no value here
+		// declaretion of a variable 
+		int numberOne; 
+		
+		// initialize with a variable 
+		numberOne = 234;
+		
+		
+		numberOne = 345; 
+		
+		numberOne = 456;
+		
+		int numberTwo ; 
+		
+		numberTwo = numberOne; 
+		
+		int y;
+		
+		
+		
+		
+	}
 
 }
 
